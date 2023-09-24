c173bc24b4b0357a245399fd0827eb50adc8a993
jurco-exe <jurajbosy1@gmail.com>
2023-09-24 19:54:13 +0200

    feat: last trafic lights programm

---
+++ main/blink_example_main.c
@@ -0,0 +1,58 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+static const char *TAG = "example";
+
+
+void app_main(void)
+{
+    // gpio_reset_pin(GPIO_NUM_2);
+    gpio_reset_pin(GPIO_NUM_4); // BTN
+    gpio_reset_pin(GPIO_NUM_18); // green
+    gpio_reset_pin(GPIO_NUM_19); // orange
+    gpio_reset_pin(GPIO_NUM_21); // red
+
+    gpio_reset_pin(GPIO_NUM_22); // red person
+    gpio_reset_pin(GPIO_NUM_23); // green person
+
+    int led_state = 0;    // the current state of LED
+    int button_state;       // the current state of button
+    int last_button_state;  // the previous state of button
+
+
+    gpio_set_direction(GPIO_NUM_18, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_19, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_21, GPIO_MODE_OUTPUT);
+    
+    gpio_set_direction(GPIO_NUM_22, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_23, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        // gpio_set_level(GPIO_NUM_18, 0);
+        // gpio_set_level(GPIO_NUM_21, 1);
+        // gpio_set_level(GPIO_NUM_19, 0);
+
+        // gpio_set_level(GPIO_NUM_23, 1);
+        // gpio_set_level(GPIO_NUM_22, 0);
+        // vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        // gpio_set_level(GPIO_NUM_18, 0);
+        // gpio_set_level(GPIO_NUM_21, 1);
+        // gpio_set_level(GPIO_NUM_19, 1);
+
+        // gpio_set_level(GPIO_NUM_23, 0);
+        // gpio_set_level(GPIO_NUM_22, 1);
+        // vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        // gpio_set_level(GPIO_NUM_18, 1);
+        // gpio_set_level(GPIO_NUM_21, 0);
+        // gpio_set_level(GPIO_NUM_19, 0);
+
+        // gpio_set_level(GPIO_NUM_23, 0);
+        // gpio_set_level(GPIO_NUM_22, 1);
+        // vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
